75b8b5f78fd176424fb74e116b040e73b0d56832
Rasmus Svensson <raek@raek.se>
2012-07-08 16:47:53 +0200

    Receive the program file path as an argument

--- Maskinspraak8000/Main.hs
+++ Maskinspraak8000/Main.hs
@@ -1,12 +1,33 @@
 module Main where
 
-import Maskinspraak8000.AST
-import Maskinspraak8000.Interpreter
-import qualified Maskinspraak8000.Parser.Lean as Parser
-import Text.Parsec.String
+import Maskinspraak8000.AST (Prog)
+import Maskinspraak8000.Interpreter (runProg, globalEnv)
+import Maskinspraak8000.Parser.Lean (program)
+import Text.Parsec.Prim (parse)
+import System.Environment (getArgs)
 
-main = do echoRes <- parseFromFile Parser.program "examples/echo.M8s"
-          case echoRes of
-              Left error -> print error
-              Right echo -> runProg echo globalEnv
+type Error = String
+
+syntax :: Error
+syntax = "Syntax: M8000 program.M8s"
+
+filePathFromArgs :: [String] -> Either Error FilePath
+filePathFromArgs args = case args of
+    [filePath] -> Right filePath
+    _          -> Left syntax
+
+parseProgram :: String -> String -> Either Error Prog
+parseProgram filePath fileContent =
+    case parse program filePath fileContent of
+        Left error -> Left $ show error
+        Right prog -> Right prog
+
+main = do args <- getArgs
+          case filePathFromArgs args of
+              Left error     -> putStrLn error
+              Right filePath -> do
+                  fileContent <- readFile filePath
+                  case parseProgram filePath fileContent of
+                      Left error -> putStrLn error
+                      Right prog -> runProg prog globalEnv
 
